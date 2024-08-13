147e35b0387ce40140dd82137c0810cfccc9cc81
tomegavazovv <tomegavazov@gmail.com>
2024-08-13 18:06:57 +0200

    s

--- main/database.py
+++ main/database.py
@@ -3,6 +3,6 @@ import firebase_admin
 from firebase_admin import credentials, db
 
 cred = credentials.Certificate(
-    'files/chatbot-eb541-firebase-adminsdk-76s8b-86eb4a55f0.json')
+    '...')
 firebase_admin.initialize_app(
     cred, {'databaseURL': '...'})
